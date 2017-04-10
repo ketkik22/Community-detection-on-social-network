bcbbc7602240b4842a6f80432454708aaedb5ff2
ketkik22 <ketki.kulkarni@sjsu.edu>
2017-04-09 23:34:16 -0700

    Commit 4

--- ModifiesLouvainAlgorithm.py
+++ ModifiesLouvainAlgorithm.py
@@ -28,8 +28,16 @@ def getClusterMembership (clusterList, noOfVertices):
                 clusterList.membership[c] = minVertex
     #return membership
 
+
+
 def getSourceAndTargetVertices(src, trgt, membership, graph):
     "This function returns correct source and destination vertex that should be connected in G'"
+    if(graph.are_connected(src, trgt)):
+        return
+    #else:
+        #for v in membership:
+
+
 
 
 """
@@ -46,82 +54,82 @@ visual_style["bbox"] = (700, 600)
 """
          Read a graph
 """
-g = Graph.Erdos_Renyi(n=25, p=0.08, directed=False, loops=False)
-#g = igraph.Graph.Read_GraphML('karate.GraphML')
+inputGraph = Graph.Erdos_Renyi(n=25, p=0.08, directed=False, loops=False)
+#inputGraph = igraph.Graph.Read_GraphML('karate.GraphML')
 
-g.vs["label"] = range(1000)
-noOfVertices = g.vcount()
+inputGraph.vs["label"] = range(1000)
+noOfVertices = inputGraph.vcount()
 
-plot(g, "originalGraph.png", **visual_style)
-print (summary(g))
+plot(inputGraph, "originalGraph.png", **visual_style)
 
-g1 = g.copy()
-g2 = g.copy()
-g1.delete_edges(g1.es())
+outputGraph = inputGraph.copy()
+copyGraph = inputGraph.copy()
+outputGraph.delete_edges(outputGraph.es())
 
 """
         Existing community detection algorithms: Louvain & Girvan-Newman
 """
-louvainCommunity = g.community_multilevel()
+louvainCommunity = inputGraph.community_multilevel()
 plot(louvainCommunity, "louvain community.png", mark_groups=True)
 #print louvainCommunity
 
-#girvanNewmanCommunity = g.community_edge_betweenness().as_clustering()
+#girvanNewmanCommunity = inputGraph.community_edge_betweenness().as_clustering()
 #plot(girvanNewmanCommunity, "girvan-newman community.png", mark_groups=True)
 
 
 """
         Finding communities till all edges in original graph are not removed
 """
-ebList = g.edge_betweenness()
+ebList = inputGraph.edge_betweenness()
 ebList = sorted(ebList, key=float)
 idx1 = 1
 alreadyUsedTuples = []
 
 while ebList:
     tupleId = 0
-#  
+#
 #      Calculating edge betweenness and finding the smallest edge betweenness ratio
 #
     for idx, eb in enumerate(ebList):
-        print ("%r ---> %f ---> %d" %(g.es[idx].tuple, eb, idx))
-        if(g.es[idx].tuple[0]!= g.es[idx].tuple[1] and g.es[idx].tuple not in alreadyUsedTuples and g.are_connected(g.es[idx].tuple[0], g.es[idx].tuple[1])):
-            alreadyUsedTuples.append(g.es[idx].tuple)            
-            tupleId = idx  
+        #print ("%r ---> %f ---> %d" %(inputGraph.es[idx].tuple, eb, idx))
+        src = inputGraph.vs.find(inputGraph.es[idx].source)
+        trgt = inputGraph.vs.find(inputGraph.es[idx].target)
+        if(src!= trgt and inputGraph.es[idx].index not in alreadyUsedTuples and inputGraph.are_connected(src, trgt)):
+            alreadyUsedTuples.append(inputGraph.es[idx].index)
+            tupleId = idx
             break
 #
 #        Adding the edge to graph G' and finding the connected components   
 #      
-    g.es[tupleId]['color'] ="green"
-    plot(g, "G%d.png" %idx1, **visual_style)
-       
-    s = g.vs.find(g.es[tupleId].source)
-    t = g.vs.find(g.es[tupleId].target)
-    src = g1.vs.find(s['label'])
-    trgt = g1.vs.find(t['label'])
+    inputGraph.es[tupleId]['color'] ="green"
+    plot(inputGraph, "G%d.png" %idx1, **visual_style)
 
-    #src = g.es[tupleId].source
-    #trgt = g.es[tupleId].target
+    s = outputGraph.vs.find(src.index)
+    t = outputGraph.vs.find(trgt.index)
+    print (src.index, "-", src['label'], "------------>", s.index, "-", s['label'])
+    print(trgt.index, "-", trgt['label'], "------------>", t.index, "-", t['label'])
 
-    #getSourceAndTargetVertices(src, trgt, componentList.membership, g2)
 
-    g1.add_edges([(src, trgt)])
-    plot(g1, "G%d'.png" %idx1, mark_groups=True)
+    #getSourceAndTargetVertices(src, trgt, componentList.membership, copyGraph)
 
-    componentList = g1.clusters(mode="WEAK")
-    getClusterMembership(componentList, noOfVertices)
-    #temp = VertexClustering( g1, membership=membership)
+    outputGraph.add_edges([(s, t)])
+    componentList = outputGraph.clusters(mode="WEAK")
+    plot(componentList, "G%d'.png" %idx1, mark_groups=True)
+
+
+    #getClusterMembership(componentList, noOfVertices)
+    #temp = VertexClustering( outputGraph, membership=membership)
     print (componentList.membership)
 #
 #       Deleting the edge and re-organizing the original graph
 #   
-    g = g2.copy()
-    g.contract_vertices(componentList.membership, combine_attrs=min)
-            
+    inputGraph = copyGraph.copy()
+    inputGraph.contract_vertices(componentList.membership, combine_attrs=min)
+
 #
 #       Re-calculating the edge betweenness ratio
 #
-    ebList = g.edge_betweenness()
+    ebList = inputGraph.edge_betweenness()
     ebList = sorted(ebList, key=float)
     #print "************************** ebList size = %d" %len(ebList)
     #print ebList
@@ -131,4 +139,4 @@ while ebList:
     print ("//////////////////////////////////////////", componentList.q)
     idx1 += 1          
 
-plot(g1, "output.png", mark_groups=True)
+plot(outputGraph, "output.png", mark_groups=True)
