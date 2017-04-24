9963144bac60d3be716c0eaf0d5436c991307a0f
ketkik22 <ketki.kulkarni@sjsu.edu>
2017-04-24 00:50:00 -0700

    Modified Louvain without meta network

--- ModifiesLouvainAlgorithmWithoutMetaNetwork.py
+++ ModifiesLouvainAlgorithmWithoutMetaNetwork.py
@@ -1,113 +1,122 @@
-import operator
 from igraph import *
-import igraph
+import queue as queue
 
+"""
+        Functions
+"""
+def getEdgeWithSmallestEdgeBetweennessRatio(g, listOfEdgesAlreadyConsidered, alreadyConsideredEBValues):
+    "This function calculates edge betweenness ratio of all edges present in graph g"
+    #print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*", listOfEdgesAlreadyConsidered)
+    temp = g.edge_betweenness()
+    edgeBetweennessList=[]
 
-#
-#       Visual style
-#
-visual_style = {}
-visual_style["edge_curved"] = False
-visual_style["vertex_color"] = "#ff3563"
-visual_style["vertex_size"] = 18
-visual_style["vertex_label_size"] = 10
-visual_style["vertex_label_color"] = "#ffffff"
-visual_style["bbox"] = (700, 600)
+    for index, eb in enumerate(temp):
+        edgeBetweennessList.insert(index,round(eb,3))
 
-visual_style1 = {}
-visual_style1["edge_curved"] = False
-visual_style1["vertex_size"] = 18
-visual_style1["vertex_label_size"] = 10
-visual_style1["vertex_label_color"] = "#ffffff"
-visual_style1["bbox"] = (700, 600)
+    temp = None
+    #print(sorted(edgeBetweennessList))
 
+    minEdgeBetweenness = max(edgeBetweennessList)
+    for eb in edgeBetweennessList:
+        if eb < minEdgeBetweenness and eb != 0.00 and eb not in alreadyConsideredEBValues:
+            minEdgeBetweenness = eb
 
-#
-#        Read a graph
-#
-#g = igraph.Graph.Read_GraphML('karate.GraphML')
-g = igraph.Graph.Erdos_Renyi(n=25, p=0.08, directed=False, loops=False)
+    #print("Min edge betweenness = %f"%minEdgeBetweenness)
+    edgeList = []
 
-g.vs["label"] = range(1000)
-plot(g, "originalGraph.png", **visual_style)
-print summary(g)
+    for index, edgeBetweenness in enumerate(edgeBetweennessList):
+        e = g.es[index]
+        #print(e.tuple, " ---------------- ",e.index," ------- %f"%edgeBetweenness)
+        if(edgeBetweenness == minEdgeBetweenness and
+           e.index not in listOfEdgesAlreadyConsidered and
+           e.source != e.target):
+            #print(e.tuple, " %%%%%%%%%%%%%%%%%%%%%%%%% ",e.index,"%%%%%%%%%%%% %f"%edgeBetweenness)
+            #print(e.tuple, " *********************** %f" % round(edgeBetweenness,3))
+            edgeList.append(e)
+    if(len(edgeList) == 0 and len(alreadyConsideredEBValues) < 10):
+        alreadyConsideredEBValues.append(minEdgeBetweenness)
+        return getEdgeWithSmallestEdgeBetweennessRatio(g, listOfEdgesAlreadyConsidered, alreadyConsideredEBValues)
+    else:
+        return edgeList
 
-g1 = g.copy()
-g2 = g.copy()
-g1.delete_edges(g1.es())
+    """
+            Visual styles
+    """
+    visual_style = {}
+    visual_style["edge_curved"] = False
+    visual_style["vertex_color"] = "#ff3563"
+    visual_style["vertex_size"] = 30
+    visual_style["vertex_label_size"] = 15
+    visual_style["vertex_label_color"] = "#ffffff"
+    visual_style["bbox"] = (1500, 1600)
 
-louvainCommunity = g.community_multilevel()
-plot(louvainCommunity, "louvain community.png", **visual_style)
-#print louvainCommunity
+    """
+             Read a graph
+    """
+    # inputGraph = Graph.Barabasi(n = 100 , m = 4, zero_appeal=3)
+    # inputGraph = Graph.Erdos_Renyi(n=25, p=0.08, directed=False, loops=False)
+    inputGraph = read('football.gml')
+    # inputGraph = Graph.Read_Edgelist('0.edges',directed=False)
+    print("%%%%%%%%%%%%%%%%",summary(inputGraph))
 
-#girvanNewmanCommunity = g.community_edge_betweenness().as_clustering()
-#plot(girvanNewmanCommunity, "girvan-newman community.png", **visual_style)
+    for v in inputGraph.vs():
+        v['label'] = v.index
 
-#
-#       Finding communities till all edges in original graph are not removed
-#
-ebList = g.edge_betweenness()
-ebList = sorted(ebList, key=float)
+    noOfVertices = inputGraph.vcount()
 
-idx1 = 0
-alreadyUsedTuples1 = []
+    plot(inputGraph, "inputGraph.png", **visual_style)
 
-while ebList:
-    tupleId = 0
-#   
-#       Calculating edge betweenness and finding the smallest edge betweenness ratio
-#    
-    for idx, eb in enumerate(ebList):
-        #print "%r ---> %f ---> %d" %(g.es[idx].tuple, eb, idx)
-        if(g.es[idx].tuple not in alreadyUsedTuples1 and g.are_connected(g.es[idx].tuple[0], g.es[idx].tuple[1])):
-            tupleId = idx
-            alreadyUsedTuples1.append(g.es[tupleId].tuple)
-            break         
-#
-#        Adding the edge to graph G' and finding the connected components   
-#      
-    g.es[tupleId]['color'] ="green"
-    
-    plot(g, "G%d.png" %idx1, **visual_style)
-    
-    s = g.vs.find(g.es[tupleId].source)
-    t = g.vs.find(g.es[tupleId].target)
-    src = g1.vs.find(s['label'])
-    trgt = g1.vs.find(t['label'])
-    
-    #src['color'] = "black"
-    #trgt['color'] = "black"
+    outputGraph = inputGraph.copy()
+    copyGraph = inputGraph.copy()
+    outputGraph.delete_edges(outputGraph.es())
 
-    g1.add_edges([(src, trgt)])
-   
-#
-#       Deleting the edge and re-organizing the original graph
-#   
-    g.delete_edges(g.es[tupleId])
-    
-#
-#       Re-calculating the edge betweenness ratio
-#
-    ebList = g.edge_betweenness()
-    ebList = sorted(ebList, key=float)
-    #print "************************** ebList size = %d" %len(ebList)
-    #print ebList
-    
-#
-#       Caclculating modularity of detected communities
-#
-    comm = g1.clusters(mode=WEAK)
-    
-    modularityScore = g1.modularity(comm, weights=None)
-    #print comm
-    print modularityScore, "---------------- at level ---------------", idx1
-    
-    plot(g1, "G%d'.png" %idx1, **visual_style1)    
-    idx1 += 1          
-            
-plot(g1, "output.png", **visual_style)
+    """
+            Existing community detection algorithms: Louvain & Girvan-Newman
+    """
+    louvainCommunity = inputGraph.community_multilevel()
+    plot(louvainCommunity, "louvain community.png", mark_groups=True)
+    print("************************* LOUVAIN ALGORITHM *************************")
+    print(summary(louvainCommunity))
+    print("Modularity of Louvain algorithm = %f\t\t" % louvainCommunity.q, "\n\n")
 
+    girvanNewmanCommunity = inputGraph.community_edge_betweenness().as_clustering()
+    plot(girvanNewmanCommunity, "girvan-newman community.png", mark_groups=True)
+    print("************************* GIRVAN-NEWMAN ALGORITHM ********************")
+    print(summary(girvanNewmanCommunity))
+    print("Modularity of Girvan-Newman algorithm = %f" % girvanNewmanCommunity.q, "\n\n\n")
 
+    isLocalMaximaAchievedCount = 30
+    maxModularity = -1
+    index = 0
+    maxModularityCommunities = []
 
+    while isLocalMaximaAchievedCount >= 0:
 
+        print("**************************************************** Iteration %d" % idx)
+        temp = []
+        edgeList = getEdgeWithSmallestEdgeBetweennessRatio(inputGraph, listOfEdgesAlreadyConsidered, temp)
+        for edge in edgeList:
+            # print(edge.tuple)
+            #tuples = getActualVertices(copyGraph, vertexCluster.membership, edge, listOfEdgesAlreadyConsidered)
+            listOfEdgesAlreadyConsidered.append(edge.index)
+            source = edge.source
+            target = edge.target
+            # print(outputGraph.vs.find(source),"+++++++++++",outputGraph.vs.find(target))
+            outputGraph.add_edges([(source, target)])
 
+        communities = outputGraph.clusters(mode="STRONG")
+
+        modularity = communities.q
+        if (maxModularity < modularity):
+            maxModularity = modularity
+            index = idx
+            maxModularityCommunities = communities.membership
+            isLocalMaximaAchievedCount = 30;
+        else:
+            isLocalMaximaAchievedCount -= 1
+
+        print("Number of communities  =  ", len(set(vertexCluster.membership)))
+        print("Modularity = %f\t\t" % (modularity))
+        # print("%d   Modularity with igraph  method = %f\t\t" % (idx, componentList.q),summary(vertexCluster))
+        plot(vertexCluster, "outputGraph%d.png" % idx, mark_groups=True)
+        idx += 1
